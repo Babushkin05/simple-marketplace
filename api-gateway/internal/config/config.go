6b598b423d0f79d8fa19960b7a9f0960f9c97f37
Vova Babushkin <babushkinvova05@gmail.com>
2025-07-21 23:02:37 +0300

    init: api-gateway config

---
+++ api-gateway/internal/config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"flag"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Server struct {
+		Port int `yaml:"port"`
+	} `yaml:"server"`
+
+	AuthService struct {
+		Addr string `yaml:"addr"`
+	} `yaml:"auth_service"`
+
+	GoodsService struct {
+		Addr string `yaml:"addr"`
+	} `yaml:"goods_service"`
+}
+
+func MustLoad() *Config {
+	path := FetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file is not exist :" + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func FetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
